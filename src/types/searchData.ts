95d38a401c32d2f3213dae2318f05fcca3865175
DevTeddy123 <ab315601@naver.com>
2025-06-20 18:17:26 +0900

    Feat : 검색 추가

---
+++ src/types/searchData.ts
@@ -0,0 +1,13 @@
+export type ResearchTopic = {
+	title: string
+	curriculum: string
+	subject: string
+	mainTopic: string
+	subTopic: string
+	difficulty: string
+	difficultyReason: string
+	faculty: string
+	keywords: string
+	relatedMajor: string
+	relatedSubject: string
+}
